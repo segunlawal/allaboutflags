78ee1b8e778574662104ec162ac403ac2b682ea3
Segun <lawalsegun72@gmail.com>
2022-10-02 01:25:22 +0100

    first commit

--- src/Pages/FlagQuiz.js
+++ src/Pages/FlagQuiz.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { ReactComponent as ComingSoon } from '../assets/comingSoon.svg';
 
 function FlagQuiz() {
   return (
     <div>
-      <p className="text-4xl text-white mt-20">COMING SOON</p>
+      <p className="text-2xl text-white mt-20 text-pink-400">COMING SOON</p>
+      <ComingSoon className="w-96 mx-auto h-96 -mt-10" />
     </div>
   );
 }
