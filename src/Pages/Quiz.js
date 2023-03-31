9b34c66fdadec9cf90e5afb205f58e5911a4c100
Segun <lawalsegun72@gmail.com>
2023-03-31 03:24:24 +0100

    feat: add flag quiz

--- src/Pages/Quiz.js
+++ src/Pages/Quiz.js
@@ -1,11 +1,113 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import { useState } from 'react';
+import './Quiz.css';
 /* eslint-disable no-console */
 
 function Quiz(props) {
   const { quizQuestions } = props;
-  console.log(quizQuestions);
+  //   console.log(quizQuestions);
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
+  const [showResult, setShowResult] = useState(false);
+  const { question, choices, correctAnswer } = quizQuestions[activeQuestion];
 
-  return <div>Quiz</div>;
+  const onClickNext = () => {
+    setSelectedAnswerIndex(null);
+    setResult(prev =>
+      selectedAnswer
+        ? {
+            ...prev,
+            score: prev.score + 1,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
+    );
+    if (activeQuestion !== quizQuestions.length - 1) {
+      setActiveQuestion(prev => prev + 1);
+    } else {
+      setActiveQuestion(0);
+      setShowResult(true);
+    }
+  };
+
+  const onAnswerSelected = (answer, index) => {
+    setSelectedAnswerIndex(index);
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true);
+    } else {
+      setSelectedAnswer(false);
+    }
+  };
+
+  const addLeadingZero = number => (number > 9 ? number : `0${number}`);
+
+  return (
+    <div className="quiz-container">
+      {!showResult ? (
+        <div>
+          <div>
+            <h1>Quiz</h1>
+            <span className="active-question-no">
+              {addLeadingZero(activeQuestion + 1)}
+            </span>
+            <span className="total-question">
+              /{addLeadingZero(quizQuestions.length)}
+            </span>
+          </div>
+          <img src={question} alt="flag" />
+          <ul>
+            {choices.map((answer, index) => (
+              <li
+                onClick={() => onAnswerSelected(answer, index)}
+                onKeyDown={event => {
+                  if (event.key === 'Enter') {
+                    onAnswerSelected(answer);
+                  }
+                }}
+                key={answer}
+                className={
+                  selectedAnswerIndex === index ? 'selected-answer' : null
+                }
+              >
+                {answer}
+              </li>
+            ))}
+          </ul>
+          <div className="flex-right">
+            <button
+              type="button"
+              onClick={onClickNext}
+              disabled={selectedAnswerIndex === null}
+            >
+              {activeQuestion === quizQuestions.length - 1 ? 'Finish' : 'Next'}
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="result">
+          <h3>Result</h3>
+          <p>
+            Total Question: <span>{quizQuestions.length}</span>
+          </p>
+          <p>
+            Total Score:<span> {result.score}</span>
+          </p>
+          <p>
+            Correct Answers:<span> {result.correctAnswers}</span>
+          </p>
+          <p>
+            Wrong Answers:<span> {result.wrongAnswers}</span>
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Quiz;
